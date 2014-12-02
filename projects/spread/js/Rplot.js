fbced4e11a7484cc8bcc6d210474e533f8bcd198
Barry Rowlingson <barry@rowlingson.com>
2014-12-02 12:56:37 +0000

    None

---
+++ projects/spread/js/Rplot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Rplot').scianimator({
+	    'images': ['spread/images/Rplot1.png', 'spread/images/Rplot2.png', 'spread/images/Rplot3.png', 'spread/images/Rplot4.png', 'spread/images/Rplot5.png', 'spread/images/Rplot6.png', 'spread/images/Rplot7.png', 'spread/images/Rplot8.png', 'spread/images/Rplot9.png', 'spread/images/Rplot10.png', 'spread/images/Rplot11.png', 'spread/images/Rplot12.png', 'spread/images/Rplot13.png', 'spread/images/Rplot14.png', 'spread/images/Rplot15.png', 'spread/images/Rplot16.png', 'spread/images/Rplot17.png', 'spread/images/Rplot18.png', 'spread/images/Rplot19.png', 'spread/images/Rplot20.png', 'spread/images/Rplot21.png', 'spread/images/Rplot22.png', 'spread/images/Rplot23.png', 'spread/images/Rplot24.png', 'spread/images/Rplot25.png', 'spread/images/Rplot26.png', 'spread/images/Rplot27.png', 'spread/images/Rplot28.png', 'spread/images/Rplot29.png', 'spread/images/Rplot30.png', 'spread/images/Rplot31.png', 'spread/images/Rplot32.png', 'spread/images/Rplot33.png', 'spread/images/Rplot34.png', 'spread/images/Rplot35.png', 'spread/images/Rplot36.png', 'spread/images/Rplot37.png', 'spread/images/Rplot38.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#Rplot').scianimator('play');
+    });
+})(jQuery);
