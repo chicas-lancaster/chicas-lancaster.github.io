4343d89ce44b4cf0d4d74300e280edc73fe34da7
Barry Rowlingson <barry@rowlingson.com>
2015-04-23 11:17:19 +0000

    None

--- events/run.js
+++ events/run.js
@@ -19,6 +19,28 @@ pad = function(v){
     };
 };
 
+function makedescription(event, max){
+    if (!event.description){ 
+	return "";
+    };
+    var trimmed = trimit(event.description, max)
+    var text = trimmed[0];
+    if(trimmed[1]){
+	text = text;
+    };
+    return [text, trimmed[1]];
+
+};
+
+
+function trimit(t, max){
+    if(!t){ return ["", false] };
+    if(t.length <= max){
+	return [t, false];
+    }
+    return [t.substr(0,max), true]
+};
+
 var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ];
 
@@ -29,7 +51,7 @@ var templates = {
     eventList: $('<div class="eventlist">'),
     eventItem: $('<div class="event"><h3 class="summary"><a href=""></a></h3><div class="start"><span class="when"/> <span class="where"/></div><div class="description"></div></div>'),
     upcomingBox: $('<div class="upcoming"><h4>Coming Next...</h4></div>'),
-    upcomingItem: $('<div class="event"><h6 class="when"/><h5 class="summary"><a title="open calendar entry" href="" target="_blank"></a></h5><p class="description"></p><div class="where"/></div>')
+    upcomingItem: $('<div class="event"><h6 class="when"/><h5 class="summary"><a title="open calendar entry" href="" target="_blank"></a></h5><p class="description"><a href="more">more</a></p><div class="where"/></div>')
 };
 
 dateFormat = function(d){
@@ -59,7 +81,7 @@ now = new Date().toISOString();
 
 api="AIzaSyAB1Jd-Jf3U-R84BJzJAPTIYZZmM1sqtjs";
 
-getN = function(n, root, wrapper, item){
+getN = function(n, root, wrapper, item, trim){
     var r = root;
     var now = new Date().toISOString();
     var URL = "https://www.googleapis.com/calendar/v3/calendars/e8j7bjqajiblsstfcc59iimss0%40group.calendar.google.com/events?callback=?&maxResults="+n+"&timeMin="+now+"&orderBy=startTime&singleEvents=true&key="+api;
@@ -75,10 +97,14 @@ getN = function(n, root, wrapper, item){
 	    }else{
 		where = " ";
 	    };
+	    var texttrim = makedescription(this, trim);
 	    e.find("a").text(this.summary).attr("href",this.htmlLink).end()
 		.find(".when").text(when).end()
 		.find(".where").text(where).end()
-		.find(".description").text(this.description);
+		.find(".description").text(texttrim[0]);
+	    if (texttrim[1]){
+		e.find(".description").append('...<a title="details" href="'+this.htmlLink+'">(more)</a>');
+	    };
 	    list.append(e);
 	});
 	$(r).append(list);
